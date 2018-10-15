eb5890e550fcc3fc337dd5ca0ce31ad5e4d4a348
Jesús Germade <jesus@germade.es>
2018-10-15 02:24:20 +0200

    better vars parsing

--- tests/parse-tests.js
+++ tests/parse-tests.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 var parseEjs = require('../lib/parse-ejs');
 
-describe('parsing', function () {
+describe('1var', function () {
 
   it('foo', function () {
 
@@ -21,6 +21,16 @@ describe('parsing', function () {
     assert.strictEqual( parseEjs(' foo <% if( foo ) { %> <%= foo %> <% } %> foobar', {
       use_with: true,
     })({ foo: 'true text' }) , ' foo  true text  foobar');
+
+  });
+
+});
+
+describe('2 vars', function () {
+
+  it('foo', function () {
+
+    assert.strictEqual( parseEjs(' foo <% if( foo ) { %> <%= bar %> <% } %> foobar')({ foo: true, bar: 'bar text' }) , ' foo  bar text  foobar');
 
   });
 
